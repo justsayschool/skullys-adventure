4ce9b6d4749b5e7755032f95d5c67d1c4eb9245d
justsayschool <80530281+justsayschool@users.noreply.github.com>
2021-04-12 09:58:11 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,12 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (true) {
+        Skully.vy = -100
+    }
+})
+let Skully: Sprite = null
 scene.setBackgroundColor(15)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+Skully = sprites.create(assets.image`Skully`, SpriteKind.Player)
+controller.moveSprite(Skully, 100, 0)
+tiles.setTilemap(tilemap`level1`)
+Skully.ay = 350
+scene.cameraFollowSprite(Skully)
