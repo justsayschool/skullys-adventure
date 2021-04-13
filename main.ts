386aab8b9f5464d2356d6bee4255f178a7b4ee46
justsayschool <80530281+justsayschool@users.noreply.github.com>
2021-04-13 10:07:46 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.over(false, effects.dissolve)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles13, function (sprite, location) {
+    game.over(false, effects.dissolve)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (true) {
-        Skully.vy = -100
+    if (Skully.vy == 0) {
+        Skully.vy = -120
     }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles15, function (sprite, location) {
+    game.over(false, effects.dissolve)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    game.over(false, effects.dissolve)
 })
 let Skully: Sprite = null
 scene.setBackgroundColor(15)
