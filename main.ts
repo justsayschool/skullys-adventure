cbd6a2f21d273fd3f869228e8e48738df6fc2d65
justsayschool <80530281+justsayschool@users.noreply.github.com>
2021-04-16 09:30:32 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const fragment = SpriteKind.create()
+    export const summon = SpriteKind.create()
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     game.over(false, effects.dissolve)
 })
@@ -9,12 +13,124 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         Skully.vy = -120
     }
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    game.over(true)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles10, function (sprite, location) {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.summon, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    bat = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    animation.runImageAnimation(
+    bat,
+    [img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c 1 b b b 1 b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b 1 f f 1 c b b b b f . . . . 
+        f f 1 f f 1 f b b b b f c . . . 
+        f f 2 2 2 2 f b b b b f c c . . 
+        . f 2 2 2 2 b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `,img`
+        . . f f f . . . . . . . . . . . 
+        f f f c c . . . . . . . . f f f 
+        f f c c c . c c . . . f c b b c 
+        f f c 3 c c 3 c c f f b b b c . 
+        f f c 3 b c 3 b c f b b c c c . 
+        f c b b b b b b c f b c b c c . 
+        c c 1 b b b 1 b c b b c b b c . 
+        c b b b b b b b b b c c c b c . 
+        c b 1 f f 1 c b b c c c c c . . 
+        c f 1 f f 1 f b b b b f c . . . 
+        f f f f f f f b b b b f c . . . 
+        f f 2 2 2 2 f b b b b f c c . . 
+        . f 2 2 2 2 2 b b b c f . . . . 
+        . . f 2 2 2 b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . c c . c c . . . . . . . . 
+        . . f 3 c c 3 c c c . . . . . . 
+        . f c 3 b c 3 b c c c . . . . . 
+        f c b b b b b b b b f f . . . . 
+        c c 1 b b b 1 b b b f f . . . . 
+        c b b b b b b b b c f f f . . . 
+        c b 1 f f 1 c b b f f f f . . . 
+        f f 1 f f 1 f b c c b b b . . . 
+        f f f f f f f b f c c c c . . . 
+        f f 2 2 2 2 f b f b b c c c . . 
+        . f 2 2 2 2 2 b c c b b c . . . 
+        . . f 2 2 2 b f f c c b b c . . 
+        . . . f f f f f f f c c c c c . 
+        . . . . . . . . . . . . c c c c 
+        `,img`
+        . f f f . . . . . . . . f f f . 
+        f f c . . . . . . . f c b b c . 
+        f c c . . . . . . f c b b c . . 
+        c f . . . . . . . f b c c c . . 
+        c f f . . . . . f f b b c c . . 
+        f f f c c . c c f b c b b c . . 
+        f f f c c c c c f b c c b c . . 
+        . f c 3 c c 3 b c b c c c . . . 
+        . c b 3 b c 3 b b c c c c . . . 
+        c c b b b b b b b b c c . . . . 
+        c 1 1 b b b 1 1 b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        f b c b b b c b b b b f . . . . 
+        . f 1 f f f 1 b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `],
+    500,
+    true
+    )
+    bat.setPosition(bat.x + 80, bat.y + 80)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles15, function (sprite, location) {
     game.over(false, effects.dissolve)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     game.over(false, effects.dissolve)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardHole, function (sprite, location) {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.fragment, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.destroy()
+})
+let bat: Sprite = null
+let summon: Sprite = null
+let shard: Sprite = null
 let Skully: Sprite = null
 scene.setBackgroundColor(15)
 Skully = sprites.create(assets.image`Skully`, SpriteKind.Player)
@@ -22,3 +138,121 @@ controller.moveSprite(Skully, 100, 0)
 tiles.setTilemap(tilemap`level1`)
 Skully.ay = 350
 scene.cameraFollowSprite(Skully)
+for (let value of tiles.getTilesByType(assets.tile`myTile7`)) {
+    shard = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c . . . . . . 
+        . . . . . c a a a a . . . . . . 
+        . . . . . a a f f b a . . . . . 
+        . . . . c a b f f c b . . . . . 
+        . . . . c b b b a f c b . . . . 
+        . . . . c b a c a b b b . . . . 
+        . . . . . b b f f a a c . . . . 
+        . . . . . . a a b b c . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.fragment)
+    animation.runImageAnimation(
+    shard,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c . . . . . . 
+        . . . . . c a a a a . . . . . . 
+        . . . . . a a f f b a . . . . . 
+        . . . . c a b f f c b . . . . . 
+        . . . . c b b b a f c b . . . . 
+        . . . . c b a c a b b b . . . . 
+        . . . . . b b f f a a c . . . . 
+        . . . . . . a a b b c . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c . . . . . . . 
+        . . . . . . . c a . . . . . . . 
+        . . . . . . . a a . . . . . . . 
+        . . . . . . c a b . . . . . . . 
+        . . . . . . c b c b . . . . . . 
+        . . . . . . c b b b . . . . . . 
+        . . . . . . . b a c . . . . . . 
+        . . . . . . . a c . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . c c . . . . . . . . 
+        . . . . . . a a a a c . . . . . 
+        . . . . . a b f f a a . . . . . 
+        . . . . . b c f f b a c . . . . 
+        . . . . b c f a b b b c . . . . 
+        . . . . b b b a c a b c . . . . 
+        . . . . c a a f f b b . . . . . 
+        . . . . . c b b a a . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c . . . . . . . 
+        . . . . . . . c a . . . . . . . 
+        . . . . . . . a a . . . . . . . 
+        . . . . . . c a b . . . . . . . 
+        . . . . . . c b c b . . . . . . 
+        . . . . . . c b b b . . . . . . 
+        . . . . . . . b a c . . . . . . 
+        . . . . . . . a c . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    900,
+    true
+    )
+    tiles.placeOnTile(shard, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+    for (let value of tiles.getTilesByType(assets.tile`myTile8`)) {
+        summon = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 6 6 . . . . . . . 
+            . . . . . . . 6 6 . . . . . . . 
+            . . . . . . 6 6 6 6 . . . . . . 
+            . . . . . . 6 9 9 6 . . . . . . 
+            . . . . . . 6 9 9 6 . . . . . . 
+            . . . . . 6 6 9 9 6 6 . . . . . 
+            . . . . . 6 9 9 9 9 6 . . . . . 
+            . . . . 6 6 9 8 8 9 6 6 . . . . 
+            . . . . 6 6 9 8 8 9 6 6 . . . . 
+            . . . 6 6 9 8 8 8 8 9 6 6 . . . 
+            . 6 6 6 9 8 8 8 8 8 8 9 6 6 6 . 
+            `, SpriteKind.summon)
+        tiles.placeOnTile(summon, value)
+        tiles.setTileAt(value, assets.tile`transparency16`)
+    }
+}
